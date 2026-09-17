5a0a7128d318e12e064ccc547b1001872c33bb9f
Eric Luis <ericluismauricio@gmail.com>
2026-09-17 20:39:50 -0300

    feat: initial commit

---
+++ backend/src/main/java/com/kiraai/backend/BackendApplication.java
@@ -0,0 +1,12 @@
+package com.kiraai.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class BackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+}
