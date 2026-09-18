3980951f8491a62041a86fd96229a28b80414c73
Eric Luis <ericluismauricio@gmail.com>
2026-09-18 01:53:43 -0300

    feat: add GoogleAuthRequestDTO

---
+++ backend/src/main/java/com/kiraai/backend/dto/GoogleAuthRequestDTO.java
@@ -0,0 +1,7 @@
+package	com.kiraai.backend.dto;
+
+public	class GoogleAuthRequestDTO {
+    private	String	idToken;
+    public	String	getIdToken()	{	return	idToken;	}
+    public	void	setIdToken(String	idToken)	{	this.idToken	=	idToken;	}
+}
