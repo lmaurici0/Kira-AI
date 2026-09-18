1e4d49e4dee5a55f83f1d1d6e4d5d1e2e1585a2b
Eric Luis <ericluismauricio@gmail.com>
2026-09-18 02:09:43 -0300

    feat: add ChatMessageRequest

---
+++ backend/src/main/java/com/kiraai/backend/dto/ChatMessageRequest.java
@@ -0,0 +1,7 @@
+package com.kiraai.backend.dto;
+
+public	class	ChatMessageRequest	{
+    private	String	content;
+    public	String	getContent()	{	return	content;	}
+    public	void	setContent(String	content)	{	this.content	=	content;	}
+}
